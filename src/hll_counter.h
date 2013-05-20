f7d8fdfd4d70bbe5ef17312f037e13f7e54eef26
Matteo Ceccarello <matteo.ceccarello@gmail.com>
2013-05-20 12:38:58 +0200

    Add more documentation

--- src/hll_counter.h
+++ src/hll_counter.h
@@ -78,6 +78,9 @@ struct hll_counter {
   hll_reg_t *registers;
 };
 
+/**
+ * @brief Type of the counter.
+ */
 typedef struct hll_counter hll_counter_t;
 
 // ----------------------------------------------------------------------------
@@ -87,13 +90,36 @@ typedef struct hll_counter hll_counter_t;
 /**
  * @brief Creates a new hyperLogLog counter, with the specified number of
  * bits to index the registers.
- * @param numRegisters
- * @return
+ *
+ * The number of registers is m = 2^bits.
+ *
+ * **Attention**: the memory for the registers of the counter is dynamically
+ * allocated. Hence, once used, the counter _must_ be deallocated with the
+ * function hll_cnt_delete(hll_counter_t).
+ *
+ * @param bits: the bits to index the registers.
+ * @return a new hll_counter_t instance
  */
 hll_counter_t hll_cnt_new(size_t bits);
 
+/**
+ * @brief Deletes a counter.
+ *
+ * This functions is responsible of deallocating the right amount of memory.
+ *
+ * @param counter the counter to be deallocated.
+ */
 void hll_cnt_delete(hll_counter_t counter);
 
+/**
+ * @brief Copies a counter.
+ *
+ * A deep copy of the counter is performed: the pointer ot the registers array
+ * is not shared.
+ *
+ * @param counter the counter to copy
+ * @return the new copy of the counter.
+ */
 hll_counter_t hll_cnt_copy(hll_counter_t counter);
 
 // ----------------------------------------------------------------------------
@@ -101,20 +127,42 @@ hll_counter_t hll_cnt_copy(hll_counter_t counter);
 // ----------------------------------------------------------------------------
 
 /**
- * Returns the position of the rightmost 1-bit.
+ * @brief Returns the position of the rightmost 1-bit in a hash.
  *
  * **Attention**: bit indexes returned by this method
  * start at 1, not at 0!
  *
- * @param x
- * @return
+ * @param elem the hash to be evaluated
+ * @param mask the bit mask to hide the first `b` bits of the hash.
+ * @return the position of the rightmost 1-bit in the hash, starting from 1.
  */
 inline hll_reg_t hll_cnt_rho(hll_hash_t elem, unsigned int mask);
 
+/**
+ * @brief Add a new element to the counter.
+ *
+ * @param elem the element to be added.
+ * @param counter the counter to be updated.
+ */
 void hll_cnt_add(hll_hash_t elem, hll_counter_t counter);
 
+/**
+ * @brief Estimates the cardinality of the underlying multiset.
+ *
+ * @param counter the counter used to get the estimation.
+ * @return the estimated cardinality of the multiset.
+ */
 hll_cardinality_t hll_cnt_size(hll_counter_t counter);
 
+/**
+ * @brief Performs theunion of two counters.
+ *
+ * The union of two counters is defined as the register by register maximum.
+ *
+ * @param a
+ * @param b
+ * @return
+ */
 hll_counter_t hll_cnt_union(hll_counter_t a, hll_counter_t b);
 
 int hll_cnt_equals(hll_counter_t a, hll_counter_t b);
