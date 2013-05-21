a3de32cb43988a60c2aeaf6183479637af4138e8
Matteo Ceccarello <matteo.ceccarello@gmail.com>
2013-05-21 19:06:12 +0200

    Start parser implementation

---
+++ src/parser.c
@@ -0,0 +1,24 @@
+#include "parser.h"
+#include <string.h>
+#include <stdio.h>
+
+void error_message(char* descr) {
+  fprintf(stderr, "Error in format of description: < %s > SKIPPING\n", descr);
+}
+
+#define MAX_ADJ_LEN 1000
+
+node_t * parse_node_descr(char *descr) {
+  int id;
+  char out_s[MAX_ADJ_LEN], in_s[MAX_ADJ_LEN];
+
+  int read =
+      sscanf(descr, "%d | %[0123456789 ] | %[0123456789 ]", &id, out_s, in_s);
+
+  if(read != 3) {
+    error_message(descr);
+  }
+
+  node_t * node = 0;
+  return node;
+}
