1247c327f6aa4d5302a7c06eae860c62aa3bbc3e
Matteo Ceccarello <matteo.ceccarello@gmail.com>
2013-05-22 23:01:59 +0200

    Reimplement node_new and node_delete
    Reimplemented both functions by means of node_init and node_free,
    to avoid code duplication.

--- src/graph.c
+++ src/graph.c
@@ -3,26 +3,13 @@
 
 node_t * node_new (node_id_t id, size_t out_n, size_t in_n) {
   node_t *n = malloc(sizeof(node_t));
-  n->id = id;
-  n->num_out = out_n;
-  n->num_in = in_n;
-  if (out_n == 0) {
-    n->out = NULL;
-  } else {
-    n->out = malloc(out_n*sizeof(node_id_t));
-  }
-  if (in_n == 0) {
-    n->in = NULL;
-  } else {
-    n->in = malloc(in_n*sizeof(node_id_t));
-  }
+  node_init(n, id, out_n, in_n);
   return n;
 }
 
 
 void node_delete (node_t *node) {
-  free(node->out);
-  free(node->in);
+  node_free(node);
   free(node);
 }
 
