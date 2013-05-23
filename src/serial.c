8ffd40866e5b9216c17758511c680462969b5fc9
Matteo Ceccarello <matteo.ceccarello@gmail.com>
2013-05-23 09:42:12 +0200

    Work on serial.c
    Parsing of the file, some error handling and allocation of the counters
    array.

--- src/serial.c
+++ src/serial.c
@@ -12,12 +12,18 @@ int main(int argc, char **argv) {
 
   int n = 0;
   node_t *nodes;
+  hll_counter_t *counters;
 
   int rc = parse_graph_file("graph.dat", &nodes, &n);
   if (rc < 0) {
     fprintf(stderr, "ERROR: could not parse graph file correctly.\n");
     exit(EXIT_FAILURE);
+  } else if (rc != n) {
+    fprintf(stderr, "ERROR: Some nodes were not parsed correctly, exiting.\n");
+    exit(EXIT_FAILURE);
   }
+
+  counters = malloc(n * sizeof(hll_counter_t));
 
   printf("Loaded graph with %d nodes\n", n);
 
