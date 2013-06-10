32a62ca88491197c55826a2ce18e817beb7aac86
Matteo Ceccarello <matteo.ceccarello@gmail.com>
2013-06-10 10:52:02 +0200

    Inline all hll_cnt_* functions

--- src/hll_counter.c
+++ src/hll_counter.c
@@ -2,7 +2,7 @@
 #include <assert.h>
 #include "check_ptr.h"
 
-void hll_cnt_init (hll_counter_t *counter, size_t bits) {
+inline void hll_cnt_init (hll_counter_t *counter, size_t bits) {
   assert( bits < sizeof(hll_hash_t)*8
          && "You cannot use more bits than there are in the hash value");
   counter->b = bits;
@@ -20,24 +20,24 @@ void hll_cnt_init (hll_counter_t *counter, size_t bits) {
   memset(counter->registers, 0, mem);
 }
 
-void hll_cnt_free (hll_counter_t * counter) {
+inline void hll_cnt_free (hll_counter_t * counter) {
   assert(counter->registers != NULL);
   free(counter->registers);
 }
 
-hll_counter_t * hll_cnt_new(size_t bits) {
+inline hll_counter_t * hll_cnt_new(size_t bits) {
   hll_counter_t *c = malloc(sizeof(hll_counter_t));
   check_ptr(c);
   hll_cnt_init(c, bits);
   return c;
 }
 
-void hll_cnt_delete(hll_counter_t * counter) {
+inline void hll_cnt_delete(hll_counter_t * counter) {
   hll_cnt_free(counter);
   free(counter);
 }
 
-hll_counter_t * hll_cnt_copy(hll_counter_t * counter) {
+inline hll_counter_t * hll_cnt_copy(hll_counter_t * counter) {
   assert(counter->registers != NULL);
   hll_counter_t *copy = malloc(sizeof(hll_counter_t));
   check_ptr(copy);
@@ -48,14 +48,14 @@ hll_counter_t * hll_cnt_copy(hll_counter_t * counter) {
   return copy;
 }
 
-void hll_cnt_copy_to (hll_counter_t *from, hll_counter_t *to) {
+inline void hll_cnt_copy_to (hll_counter_t *from, hll_counter_t *to) {
   assert(from->b == to->b);
   assert(from->registers != NULL);
   assert(to->registers != NULL);
   memcpy(to->registers, from->registers, from->m*sizeof(hll_reg_t));
 }
 
-hll_reg_t hll_cnt_rho(hll_hash_t x, hll_hash_t mask) {
+inline hll_reg_t hll_cnt_rho(hll_hash_t x, hll_hash_t mask) {
   if( (x & mask) == 0 ) {
     return sizeof(hll_hash_t)*8;
   }
@@ -67,7 +67,7 @@ hll_reg_t hll_cnt_rho(hll_hash_t x, hll_hash_t mask) {
   return i;
 }
 
-void hll_cnt_add(hll_hash_t x, hll_counter_t * cnt) {
+inline void hll_cnt_add(hll_hash_t x, hll_counter_t * cnt) {
   assert(cnt->registers != NULL);
   // Computes the register index
   size_t j = (cnt->b == 0)? 0 : x >> (sizeof(hll_hash_t)*8 - cnt->b);
@@ -76,7 +76,7 @@ void hll_cnt_add(hll_hash_t x, hll_counter_t * cnt) {
   cnt->registers[j] = (cnt->registers[j] > r)? cnt->registers[j] : r;
 }
 
-hll_cardinality_t hll_cnt_size(hll_counter_t * counter) {
+inline hll_cardinality_t hll_cnt_size(hll_counter_t * counter) {
   assert(counter->registers != NULL);
   double denominator = 0;
   int i;
@@ -93,7 +93,7 @@ hll_cardinality_t hll_cnt_size(hll_counter_t * counter) {
 
 #define MAX(a, b) (a >= b)? a : b
 
-hll_counter_t * hll_cnt_union(hll_counter_t * a, hll_counter_t * b) {
+inline hll_counter_t * hll_cnt_union(hll_counter_t * a, hll_counter_t * b) {
   assert(a->b == b->b);
   assert(a->registers != NULL);
   assert(b->registers != NULL);
@@ -105,7 +105,7 @@ hll_counter_t * hll_cnt_union(hll_counter_t * a, hll_counter_t * b) {
   return c;
 }
 
-void hll_cnt_union_i(hll_counter_t *a, hll_counter_t *b) {
+inline void hll_cnt_union_i(hll_counter_t *a, hll_counter_t *b) {
   assert(a->b == b->b);
   assert(a->registers != NULL);
   assert(b->registers != NULL);
@@ -117,7 +117,7 @@ void hll_cnt_union_i(hll_counter_t *a, hll_counter_t *b) {
 
 #undef MAX
 
-int hll_cnt_equals(hll_counter_t * a, hll_counter_t * b) {
+inline int hll_cnt_equals(hll_counter_t * a, hll_counter_t * b) {
   assert(a->registers != NULL);
   assert(b->registers != NULL);
   if(a->b != b->b) {
